b142fe1e041f15ae8f1fc5aa0c922f30b2f39d8b
filtcgil <archiviofiltcgil@gmail.com>
2026-01-20 16:41:56 +0100

    Delete android directory

--- android/app/src/main/kotlin/com/filt/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.filt.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/filt/filt_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.filt.filt_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
